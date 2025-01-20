165fa1ec7c3a1575e08235dd812cf417fc182aed
Robert Evans <robermar2@gmail.com>
2025-01-20 15:43:26 -0500

    detect platform for windows and linux support

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pygame
 from pathlib import Path
 
@@ -18,12 +19,38 @@ def setup_display(display: str, video_driver: str, screen_width: int, screen_hei
     os.putenv("DISPLAY", display)
     os.putenv("SDL_VIDEODRIVER", video_driver)
 
-    pygame.display.init()
-    size = (screen_width, screen_height)
-    screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-    screen.fill(BLACK)
-    pygame.font.init()
-    pygame.display.update()
+    pygame.init()
+    
+    # Detect platform
+    platform = sys.platform
+    print(f"Running on platform: {platform}")
+    if platform.startswith("linux"):
+        print("Running on Linux, using framebuffer")
+        pygame.display.init()
+        size = (screen_width, screen_height)
+        screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+        screen.fill(BLACK)
+        pygame.font.init()
+        pygame.display.update()
+    elif platform.startswith("win"):
+        print("Running on Windows")
+        os.putenv("SDL_VIDEODRIVER", "")
+        pygame.display.init()
+        # Get screen resolution of the selected display
+        display_info = pygame.display.Info()
+        #print(f"Display info: {display_info}")
+        width = display_info.current_w
+        display_index = int(display)
+        # Position the window manually to support 2 displays atm
+        if display_index == 1:  # Second monitor
+            os.environ['SDL_VIDEO_WINDOW_POS'] = f"{width},0"  # Place at the start of the second screen
+        else:  # Primary monitor
+            os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
+
+        screen = pygame.display.set_mode((screen_width, screen_height), pygame.NOFRAME)
+        screen.fill(BLACK)
+        pygame.font.init()
+        pygame.display.update()
 
     #this screen object is what is used to create everything else
     return screen 
